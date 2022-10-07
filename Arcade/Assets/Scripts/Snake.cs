e3733461b95f32019965b850ed2eb9e19bb05f70
ThiagoPereira232 <pereira.tp.thiago@gmail.com>
2022-10-07 18:01:19 -0300

    snake grow

--- Arcade/Assets/Scripts/Snake.cs
+++ Arcade/Assets/Scripts/Snake.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Snake : MonoBehaviour
 {
     private Vector2 _direction = Vector2.right;
+    private List<Transform> _segments;
+    public Transform segmentPrefab;
+
+    private void Start()
+    {
+        _segments = new List<Transform>();
+        _segments.Add(this.transform);
+    }
 
     private void Update()
     {
@@ -25,6 +33,27 @@ public class Snake : MonoBehaviour
 
     private void FixedUpdate()
     {
+        for(int i = _segments.Count -1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
         this.transform.position = new Vector3(Mathf.Round(this.transform.position.x) + _direction.x, Mathf.Round(this.transform.position.y) + _direction.y, 0.0f);
+    }
+
+    void Grow()
+    {
+        Transform segment = Instantiate(segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+        _segments.Add(segment);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Food")
+        {
+            Grow();
+
+        }
     }
 }
